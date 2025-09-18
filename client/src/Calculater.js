71276dfa7efe237e2a91cf6f05398fe69c92d104
Montse58 <montsealonso2001@gmail.com>
2025-09-17 21:46:02 -0700

    added a transparent picture for title logo. Added the logo to the website tab. Centered the title to the logo.

--- client/src/Calculater.js
+++ client/src/Calculater.js
@@ -6,11 +6,22 @@ function Calculater() {
     <div className='Calculater'>
       <div className='Header'>
         <div className='Logo'>
+          {/*Robot logo */}
+          <img src="/RobotIcon2.png" alt="Robot Icon" className="RobotIcon" />
 
         </div>
         <div className='Calc-Title'>
           CALCU-LATER
         </div>
+    </div>
+
+    <div className='SubHeader'>
+      <div className='From-Section'>
+
+      </div>
+      <div className='To-Section'>
+        
+      </div>
     </div>
     </div>
   );
